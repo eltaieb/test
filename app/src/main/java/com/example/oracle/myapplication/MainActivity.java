a235f6a321559c2b3013aa579db1ef5ba2fb66b8
j <javaAdore@gmail.com>
2016-04-09 02:52:09 -0700

    Modified by mahmoud eltaieb .. change welcome message to be hello to my project

--- app/src/main/java/com/example/oracle/myapplication/MainActivity.java
+++ app/src/main/java/com/example/oracle/myapplication/MainActivity.java
@@ -70,7 +70,7 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
             Toast.makeText(getApplicationContext(),R.string.main_activity_enter_name_validation, Toast.LENGTH_LONG).show();
         }else
         {
-            Toast.makeText(getApplicationContext()," Hello,  "+  name, Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext()," Hello,  to  my project ya "+  name, Toast.LENGTH_LONG).show();
 
         }
     }
